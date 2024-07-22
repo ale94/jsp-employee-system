62e1d1b2f7d73452e249441d78879d13d6822571
ale94 <alejandrorua111@gmail.com>
2024-07-21 21:04:34 -0300

    Revert "Revert "fix: controller correction""
    This reverts commit f0908fa9eb82a4ac42f18fbd5a0250b1c9082f53.

--- src/main/java/com/ale94/employees/controller/IndexController.java
+++ src/main/java/com/ale94/employees/controller/IndexController.java
@@ -1,12 +1,15 @@
 package com.ale94.employees.controller;
 
+import com.ale94.employees.entity.EmployeeEntity;
 import com.ale94.employees.services.IEmployeeService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @AllArgsConstructor
@@ -21,6 +24,39 @@ public class IndexController {
         employees.forEach(e -> log.info(String.valueOf(e)));
         model.put("employees", employees);
         return "index";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String showAdd() {
+        return "add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String add(@ModelAttribute("employeeForm") EmployeeEntity employee) {
+        log.info(String.valueOf(employee));
+        service.create(employee);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.GET)
+    public String showEdit(@RequestParam Long id, ModelMap model) {
+        var employee = service.getById(id);
+        log.info("Empleado a editar: {}", employee);
+        model.put("employee", employee);
+        return "edit";
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    public String edit(@ModelAttribute("employeeForm") EmployeeEntity employee) {
+        log.info("String.valueOf(employee)");
+        service.create(employee);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.GET)
+    public String showEdit(@RequestParam Long id) {
+        service.delete(id);
+        return "redirect:/";
     }
 
 }
